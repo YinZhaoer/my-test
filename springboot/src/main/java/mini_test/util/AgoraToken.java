06db680d5271e089049788867a1f6713b4cf68a8
zhaoyin <zhaoyin@corp.netease.com>
2019-07-25 16:31:20 +0800

    修改JwtUtil

---
+++ springboot/src/main/java/mini_test/util/AgoraToken.java
@@ -0,0 +1,13 @@
+package mini_test.util;
+
+/**
+ * Author: ZhaoYin
+ * Date: 2019/7/25
+ * Time: 14:23
+ * Created with IntelliJ IDEA
+ * Description:
+ */
+public class AgoraToken {
+    
+
+}
--- springboot/src/main/java/mini_test/util/JwtUtil.java
+++ springboot/src/main/java/mini_test/util/JwtUtil.java
@@ -10,29 +10,70 @@ package mini_test.util;
 
 
 import io.jsonwebtoken.*;
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Component
 public class JwtUtil {
+    private static final String issuer="babao";
 
-    @Value("${jwt.key}")
-    private String key;
+    //秘钥Key
+    private static final String JWTKey = "022bdc63c3c5a45879ee6581508b9d03adfec4a4658c0ab3d722e50c91a351c42c231cf43bb8f86998202bd301ec52239a74fc0c9a9aeccce604743367c9646b";
 
-    public String createJws(String info) {
-        String jws = Jwts.builder()
-                .setHeaderParam("alg", "HS256")
-                .setHeaderParam("type", "jwt")
-                .setId(info)
-                .signWith(SignatureAlgorithm.HS256, key)
-                .compact();
-        return jws;
+    private static SecretKey secretKey() {
+        byte[] encodedKey = Base64.decodeBase64(JWTKey);
+        return new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
+    }
+
+
+    /**
+     * JWT创建函数
+     *
+     * @param id        JWTid
+     * @param issuer    JWT签发人
+     * @param subject   JWT所有人信息
+     * @param ttlMillis JWT过期时间
+     * @return JWT
+     * @throws Exception JWT创建异常
+     */
+    public static String createJWT(String id, String subject, long ttlMillis) throws Exception {
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date nowDate = new Date(nowMillis);
+
+        Map<String, Object> claims = new HashMap<>();
+
+
+        SecretKey secretKey = secretKey();
+
+        JwtBuilder builder = Jwts.builder()
+                .setClaims(claims)
+                .setId(id)
+                .setIssuer(issuer)
+                .setIssuedAt(nowDate)
+                .setSubject(subject)
+                .signWith(signatureAlgorithm, secretKey);
+
+        if (ttlMillis >= 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date expDate = new Date(expMillis);
+            builder.setExpiration(expDate);
+        }
+
+        return builder.compact();
     }
 
     public boolean validateJws(String jws) {
         try {
-            Jwts.parser().setSigningKey(key).parseClaimsJws(jws);
+            Jwts.parser().setSigningKey(JWTKey).parseClaimsJws(jws);
             return true;
         } catch (JwtException | IllegalArgumentException e) {
             throw new JwtException("Expired or invalid JWT token");
